26fe685bc4001c9e90d5b457d67408eeed72d1af
Jkr2255 <magnesium.oxide.play@gmail.com>
2015-11-22 20:01:53 +0900

    refactoring (using moduling)

---
+++ Rakefile
@@ -0,0 +1,33 @@
+require 'rake/clean'
+
+source_js = FileList['src/**/*.js']
+source_coffee = FileList['src/**/*.coffee']
+
+tmp_js = source_js.pathmap("%{^src,tmp}p") + source_coffee.pathmap("%{^src,tmp}X.js")
+
+CLEAN.include tmp_js
+
+build_top = FileList['src/*.js', 'src/*.coffee']
+
+build_top_tmp = build_top.pathmap("%{^src,tmp}X.js")
+
+dist_top = build_top.pathmap("%{^src,gen}X.js")
+
+CLOBBER.include dist_top
+
+task default: dist_top
+
+rule /gen\/.+\.js/ => tmp_js do |t|
+  sh "browserify #{t.name.pathmap('%{^gen,tmp}p')} | uglifyjs -cm > #{t.name}"
+end
+
+rule /tmp\/.+\.js/ => "%{^tmp,src}p" do |t|
+  mkpath t.name.pathmap('%d')
+  sh "babel #{t.source} -o #{t.name}"
+end
+
+rule /tmp\/.+\.js/ => "%{^tmp,src}X.coffee" do |t|
+  path = t.name.pathmap('%d')
+  mkpath path
+  sh "coffee -bco #{path} #{t.source}"
+end
---
+++ src/lib/sat_solver.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var SatConstraints = require('./sat_constraints');
+
+var _map = require('lodash/collection/map');
+
+class SatSolver{
+
+	loadWorker(){
+		this.worker = new Worker('gen/minisat_worker.js');
+	}
+	
+	setListener(func){
+		if(!this.worker){
+			this.loadWorker();
+		}
+		if(this.listener){
+			this.worker.removeEventListener('message',this.listener, true);
+		}
+		this.listener = function(e) {
+			var result = e.data.result;
+			var result_arr = null;
+			var result_status = 'ERROR';
+			
+			if (result) {
+				result_arr = result.split(' ');
+				result_status = result_arr.shift();
+				result_arr = _map(result_arr, x => +x);
+			}
+			func(result_status, result_arr, e.data.stdout, e.data.stderr);
+		}
+		this.worker.addEventListener('message',this.listener, true);
+	}
+	
+	solve(constraints, callback){
+		var dimac_text = constraints.toDIMACS();
+		this.setListener(callback);
+		this.worker.postMessage(dimac_text);
+	}
+	
+	//コールバックの返り値:falseならループ終了、SatConstraintsならそれをもとに次のループへ
+	//とりあえずそれ以外はTypeError
+	solve_loop(constraints, callback){
+		this.setListener(function(result_status, result_arr, stdout, stderr) {
+			var ret = callback(constraints, result_status, result_arr, stdout, stderr);
+			if (ret === false){
+				return;
+			}
+			if (ret instanceof SatConstraints){
+				constraints = ret;
+				this.worker.postMessage(ret.toDIMACS());
+				return;
+			}
+			throw new TypeError;
+		});
+		this.worker.postMessage(constraints.toDIMACS());
+	}
+	
+};
+
+module.exports = SatSolver;
